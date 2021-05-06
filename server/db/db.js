658a958703146ee1d2f5a337b5d7b8daf7778000
SERGE <nsergearistide@gmail.com>
2021-05-06 18:59:30 -0400

    deleted procfile and added ssl config to database

--- server/db/db.js
+++ server/db/db.js
@@ -3,12 +3,29 @@ const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
-const db = new Sequelize(
-  process.env.DATABASE_URL ||
-    `postgres://postgres:1992@localhost:5432/${databaseName}`,
-  {
+let config
+
+if (process.env.DATABASE_URL) {
+  config = {
+    logging: false,
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+} else {
+  config = {
     logging: false
   }
+}
+
+const db = new Sequelize(
+  process.env.DATABASE_URL ||
+    `postgres://serge:1234@localhost:5432/${databaseName}`,
+  config
 )
 module.exports = db
 
