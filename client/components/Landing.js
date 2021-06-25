fe80dff1fe4d6cf056bc339812ab23a2dbb9046c
SERGE <nsergearistide@gmail.com>
2021-06-24 21:08:32 -0400

    fixed cart content naming and landing page responsivness

--- client/components/Landing.js
+++ client/components/Landing.js
@@ -1,18 +1,39 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
 import Box from '@material-ui/core/Box'
+import {makeStyles} from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+  box: {
+    '@media (max-width: 600px)': {
+      width: '50%',
+      marginLeft: '30px',
+      marginTop: '2px'
+    }
+  },
+  buttonStyle: {
+    '@media (max-width: 600px)': {
+      fontSize: '5px',
+      color: 'red',
+      backgroundColor: 'white'
+    }
+  }
+})
 
 function Landing() {
+  const classes = useStyles()
+
   return (
     <div className="landing-page">
       <div className="box1">
         <p className="landing-paragraph">
           The Party <br />
-          <p>Start Right Here</p>{' '}
+          Start Right Here
         </p>
 
-        <Box mt={15} ml={11} fS={50}>
+        <Box mt={15} ml={11} fS={50} className={classes.box}>
           <Button
+            className={classes.buttonStyle}
             variant="contained"
             color="primary"
             disableElevation
